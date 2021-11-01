c92e239d5015e89d9fef1699a3962ef458e334ae
adrshojk <adrshojk@gmail.com>
2021-11-01 13:16:29 +0530

    header component added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
-
+import Header from './components/Header';
+import { BrowserRouter as Router } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
-      DRIVER LOGISTICS
+      <Router>
+        <Header />
+
+      </Router>
     </div>
   );
 }
