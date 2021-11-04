a25a6c0f6ae110b502305aef714a35910b4fc7d2
bhgath <bhagath@trois.in>
2021-11-04 22:57:51 +0530

    service

---
+++ src/components/about/AboutUsHead.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import about_inner_img from '../../assets/images/about_inner_img_1.png';
+
+
+function AboutUsHead() {
+    return (
+        <section className="inner_aboutsection">
+
+            <div className="container">
+
+                <div className="col-md-12">
+
+                    <div className="row">
+
+                        <div className="col-md-6">
+
+                            <div className="inner_aboutsection_contantpart wow fadeInUp" data-wow-delay="1s">
+
+                                <h1>About <span className="banner_redfont">US</span></h1>
+                                <p>Driver Logistics has a mission to assist the most
+                                successful companies in the world to empower their
+                                    supply chain.</p>
+
+                                <p>We understand that growing companies have their
+                                priorities and core functions to take care of. Managing
+                                logistics operations is a troublesome task for
+                                businesses. This is where we come into the picture. We
+                                have the experience, network, and technology to take
+                                    care of a company's B2B fulfillment and logistics.</p>
+
+                                <p>We are a team of unwavering visionaries and
+                                unstoppable doers. We focus on finding new ways to
+                                help our customers succeed while incessantly
+                                    improving ourselves.</p>
+
+                            </div>
+
+                        </div>
+
+
+                        <div className="col-md-6">
+
+                            <div className="inner_aboutsection_contantpart wow fadeInUp" data-wow-delay="1s">
+
+                                <img src={about_inner_img}></img>
+
+                            </div>
+
+                        </div>
+
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </section>
+    )
+}
+
+export default AboutUsHead
---
+++ src/components/about/OurVision.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+function OurVision() {
+    return (
+        <section className="mission_section">
+
+            <div className="container">
+
+                <div className="col-md-12 wow fadeInUp" data-wow-delay="1s">
+
+                    <h2 className="background_double"><span className='h2_Span'>OUR VISION</span></h2>
+
+                    <p>Driver Logistics has a mission to assist the most successful companies in the world to empower
+                    their supply
+                        chain.</p>
+
+                    <p>We understand that growing companies have their priorities and core functions to take care of.
+                    Managing
+                    logistics operations is a troublesome task for businesses. This is where we come into the
+                    picture. We have the
+                    experience, network, and technology to take care of a company's B2B fulfillment and logistics.
+                    </p>
+
+                    <p>We are a team of unwavering visionaries and unstoppable doers. We focus on finding new ways to
+                    help our
+                        customers succeed while incessantly improving ourselves.</p>
+
+                </div>
+
+            </div>
+
+        </section>
+    )
+}
+
+export default OurVision
