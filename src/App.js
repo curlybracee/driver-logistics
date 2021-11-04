cde136108f1837546234ec7df217ce3725a84277
bhgath <bhagath@trois.in>
2021-11-04 15:57:45 +0530

    About as

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import React from 'react'
 import './App.css';
 import Header from './components/Header';
 import Loader from './components/Loader';
-import Footer from './components/Footer';
+import AboutUs from './components/AboutUs';
 import { BrowserRouter as Router } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
       <Router>
-        <Header />
-        <Footer />
+        <AboutUs />
+        {/* <Footer /> */}
       </Router>
     </div>
   );
