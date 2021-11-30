60eaa2d880337442a66678354b98117476c01c20
bhgath <bhagath@trois.in>
2021-11-30 16:41:31 +0530

    react redux

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { compose, createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import { rootReducer } from './redux/reducer/rootReducer';
+import createSagaMiddleware from 'redux-saga';
+import commonSaga from './redux/commonSaga';
+
 import './index.css';
 import App from './App';
 
+const sagaMiddleware = createSagaMiddleware()
+const composeEnhancer = typeof window === 'object' && window['__REDUX_DEVTOOLS_EXTENSION_COMPOSE__'] ?
+  window['__REDUX_DEVTOOLS_EXTENSION_COMPOSE__']({}) : compose
+const store = createStore(rootReducer,
+  composeEnhancer(applyMiddleware(sagaMiddleware))
+);
+store.runSaga = sagaMiddleware.run
+store.runSaga(commonSaga)
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
