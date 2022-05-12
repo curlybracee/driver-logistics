524a6379a791543bdb78709d145a504ad633662d
bhagath96 <bhagathrudolf@gmail.com>
2022-05-12 17:29:05 +0530

    h2

--- src/components/home/BelowService.js
+++ src/components/home/BelowService.js
@@ -15,20 +15,19 @@ const BelowService = () => {
             <Container className="container">
                 <div className="service_contantbox_outer">
                     <div className="service_box service_img_right">
-                        <h1 style={{
+                        <h2 style={{
                             textAlign: 'center',
                             width: '100%',
                             paddingLeft: '40px',
                             paddingRight: '40px',
-                            lineHeight: '33px',
                             marginBottom: '20px',
                             fontFamily: 'sans-serif'}}>
                             Best in Class Logistics Company in Kochi,Kerala<br /> with Pan India
                             Presence
-                        </h1>
-                        <h3>
+                        </h2>
+                        <h4>
                             Kerala’s leading logistics company for comprehensive 3PL services.
-                        </h3>
+                        </h4>
                         <p>
                             Driver Logistics gained a paramount name among logistics companies in Kerala by delivering outright and customised logistics and warehouse solutions for clients, enabling them to save on time and cost.
 
